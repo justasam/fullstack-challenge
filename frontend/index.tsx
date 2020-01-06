a8f08a8c577ef23e11cbb1476a7197f301cc0ce1
Justas Samuolis <samuolis.just@gmail.com>
2020-01-06 08:33:26 +0000

    Add some styling

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./routes";
+import './index.css';
 
 ReactDOM.render(
   <Router>
--- frontend/views/Home.tsx
+++ frontend/views/Home.tsx
@@ -1,7 +1,53 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getAllCakes } from "../api";
 
 import { Cake } from "../interfaces";
+
+interface CakeLinkProps {
+  cake: Cake;
+}
+
+const CakeLink = (props: CakeLinkProps) => {
+  return (
+    <Link
+      to={`/cake/${props.cake.id}`}
+      style={{
+        textDecoration: "none",
+        color: "black",
+        position: "relative",
+        display: "flex",
+        maxWidth: "500px",
+        margin: "20px auto",
+        flexDirection: "column",
+        borderRadius: 5,
+        border: "1px solid #ddd",
+        boxShadow: '0px 4px 8px rgba(0,0,0,.15)'
+      }}
+    >
+      <img
+        src={props.cake.imageUrl}
+        alt={props.cake.name}
+        style={{
+          objectFit: "cover",
+          maxWidth: "100%",
+          borderRadius: 5
+        }}
+      />
+      <h4
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          backgroundColor: "rgba(255,255,255,.5)",
+          padding: 10
+        }}
+      >
+        {props.cake.name}
+      </h4>
+    </Link>
+  );
+};
 
 const Home = () => {
   const [cakes, setCakes] = useState([]);
@@ -29,12 +75,7 @@ const Home = () => {
       ) : error ? (
         <h2>{error}</h2>
       ) : (
-        cakes.map((cake: Cake) => (
-          <div key={cake.id}>
-            <img src={cake.imageUrl} alt={cake.name} />
-            <h2>{cake.name}</h2>
-          </div>
-        ))
+        cakes.map((cake: Cake) => <CakeLink cake={cake} key={cake.id} />)
       )}
     </div>
   );
